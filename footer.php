97f8f14a94962c1a97d57240f7f36faaf2c8ab11
ALIF101XL <alif101xl@gmail.com>
2026-05-29 19:26:57 +0700

    Web Tools V2

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+  <div class="d-sm-flex justify-content-center justify-content-sm-between">
+    <span class="text-muted d-block text-center text-sm-left d-sm-inline-block">Copyright © 2025 ANDRAX.OffSec.Id <?php echo date('Y') ?> </span>
+
+  </div>
+</footer>
